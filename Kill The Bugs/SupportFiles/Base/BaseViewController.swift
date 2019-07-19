f759c11c5bd000d19bbf14e2f16d42068c9797d4
fadil <fadil_boyz@ymail.com>
2019-07-19 23:31:44 +0700

    improve attack button

--- Kill The Bugs/SupportFiles/Base/BaseViewController.swift
+++ Kill The Bugs/SupportFiles/Base/BaseViewController.swift
@@ -28,7 +28,26 @@ class BaseViewController: UIViewController {
     @discardableResult
     func showAttackCommand(withView view: UIView, andTargetView targetView: UIView) -> PopTip {
         let popTip = PopTip()
-        popTip.show(text: "Attack!", direction: .up, maxWidth: 200, in: view, from: targetView.globalFrame!, duration: nil)
+        popTip.cornerRadius = 22.0
+        popTip.padding = 0
+        popTip.bubbleColor = .white
+        
+        let customView = UIView(frame: CGRect(x: 0, y: 0, width: 100.0, height: 44.0))
+        let attackButton = UIImageView(frame: CGRect(x: 0, y: 0, width: 100.0, height: 44.0))
+        attackButton.animationImages = [UIImage(named: "empty-button-normal")!, UIImage(named: "empty-button-click")!]
+        attackButton.animationDuration = 1.5
+        attackButton.startAnimating()
+        let attackLabel = UILabel(frame: CGRect(x: 5, y: 5, width: 90.0, height: 34.0))
+        attackLabel.font = UIFont(name: "VIDEOPHREAK", size: 15.0)
+        attackLabel.textAlignment = .center
+        attackLabel.textColor = .white
+        attackLabel.shadowColor = .darkGray
+        attackLabel.text = "ATTACK!"
+        
+        customView.addSubview(attackButton)
+        customView.addSubview(attackLabel)
+        
+        popTip.show(customView: customView, direction: .up, in: view, from: targetView.globalFrame!)
         
         return popTip
     }
